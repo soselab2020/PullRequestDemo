3f4308a422dc6e145ded728a8409bed977b00efd
soselab2020 <71771263+soselab2020@users.noreply.github.com>
2022-09-19 23:29:33 +0800

    fix: add log

--- demo_querystring.js
+++ demo_querystring.js
@@ -6,5 +6,6 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     let q = url.parse(req.url, true).query;
     let txt = q.year + " " + q.month;
+    console.log(q + " " + txt);
     res.end(txt);
 }).listen(8080);
